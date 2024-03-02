408e1db6014b11884b393cbde01541f569111cfc
AzamatUzakov <azamatakmal053@gmail.com>
2024-03-02 15:21:47 +0500

    crate vite project and finished prseta page

---
+++ front/components/prseta/index.js
@@ -0,0 +1,21 @@
+/* 
+let posters__contener = document.querySelector('.flex__box')
+
+
+for (let item = 0; item < 3; item++) {
+    let main__poster__box = document.createElement('div')
+    main__poster__box.classList.add("main__poster__box")
+    main__poster__box.innerHTML = ` 
+        <div class="poster__bg"><img src="/public/poster_bg.png" alt="">
+            <div class="player"><img src="/public/player.png" alt=""></div>
+        </div>
+        <div class="title__box">
+            <p>$35,00 <br>
+                ARTIST STARTER KIT</p>
+
+            <img width="30%" src="/public/stars (2).png" alt="">
+        </div>    
+`
+    posters__contener.append(main__poster__box)
+}
+ */
