532cb67ea9ddb15b7dca0379ddfc4191c642f48c
halareka <149045655+halareka@users.noreply.github.com>
2024-03-04 00:47:26 +0700

    feat : add categories

---
+++ categories/front/components/header/index.js
@@ -0,0 +1,7 @@
+let header = document.querySelector('header')
+
+header.innerHTML = ` <h2>PRESET S STAR</h2>
+<div class="acount_box">
+    <img src="/public/acount.png" alt="">
+    <img src="/public/basket.png" alt="">
+</div>`
