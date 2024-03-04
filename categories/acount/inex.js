7fd21189b7a0a37db1a04ca80b67cd272e7a7845
AzamatUzakov <azamatakmal053@gmail.com>
2024-03-04 17:53:28 +0500

    fix

---
+++ categories/acount/inex.js
@@ -0,0 +1,9 @@
+let account__logo = document.querySelector('.acc_logo')
+let modal__register = document.querySelector('.main__authorization__contener')
+console.log(modal__register);
+console.log(account__logo);
+account__logo.onclick = () => {
+    modal__register.style.display = "block"
+    console.log("clcikc");
+    }
+    
--- categories/front/components/header/index.js
+++
@@ -1,7 +0,0 @@
-let header = document.querySelector('header')
-
-header.innerHTML = ` <h2>PRESET S STAR</h2>
-<div class="acount_box">
-    <img src="/public/acount.png" alt="">
-    <img src="/public/basket.png" alt="">
-</div>`
