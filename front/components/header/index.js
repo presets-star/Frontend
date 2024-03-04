635492a317d32519cae9a03624f08b2349187570
AzamatUzakov <azamatakmal053@gmail.com>
2024-03-04 16:54:55 +0500

    finish register

--- front/components/header/index.js
+++ front/components/header/index.js
@@ -1,7 +1,16 @@
 let header = document.querySelector('header')
+let modal__register = document.querySelector('.main__authorization__contener')
+console.log(modal__register);
 
-header.innerHTML = ` <h2>PRESET S STAR</h2>
+header.innerHTML = `<h2>PRESET S STAR</h2>
 <div class="acount_box">
-    <img src="/public/acount.png" alt="">
+    <img class="register_img" src="/public/acount.png" alt="">
     <img src="/public/basket.png" alt="">
 </div>`
+
+let register__btns = document.querySelector('.register_img')
+register__btns.onclick = () => {
+modal__register.style.display = "block"
+console.log("clcikc");
+}
+
