35497937eba3709036e673fd8f4b077cebd2351c
AzamatUzakov <azamatakmal053@gmail.com>
2024-03-07 22:12:33 +0500

    .

--- front/components/profile/index.js
+++ front/components/profile/index.js
@@ -12,6 +12,7 @@ let switching__favorites = document.querySelector('.switching__favorites')
 let switching__portfolio = document.querySelector('.switching__portfolio')
 console.log(pagesBtn);
 
+let modal_create_prseta = document.querySelector('.main__newPrseta__contener')
 
 pagesBtn.forEach(btn => {
 
@@ -67,13 +68,15 @@ switchingBtn.forEach(btn => {
 
 
 
-let posters__contener = document.querySelector('.flex__box')
 
 
-for (let item = 0; item < 8; item++) {
-    let main__poster__box = document.createElement('div')
-    main__poster__box.classList.add("main__poster__box")
-    main__poster__box.innerHTML = ` 
+function favoritesPage(arr) {
+    let favoriteFlex__box = document.querySelector('.flex__box')
+
+    for (let item = 0; item < 5; item++) {
+        let main__poster__box = document.createElement('div')
+        main__poster__box.classList.add("main__poster__box")
+        main__poster__box.innerHTML = ` 
         <div class="poster__bg"><img src="/public/poster_bg.png" alt="">
             <div class="player"><img src="/public/player.png" alt=""></div>
         </div>
@@ -87,5 +90,69 @@ for (let item = 0; item < 8; item++) {
 <div class="btn__cont">
         <button class="basket__btn">В корзину </button>
         </div>`
-    posters__contener.append(main__poster__box)
+        favoriteFlex__box.append(main__poster__box)
+    }
 }
+favoritesPage()
+
+function portfolioPage(arr) {
+    let portfolioFlex__box = document.querySelector('.portfolio-flex__box')
+    let create__box = document.querySelector('.create__box')
+
+    for (let item = 0; item < 8; item++) {
+        let main__poster__box = document.createElement('div')
+        main__poster__box.classList.add("main__poster__box")
+        main__poster__box.innerHTML = ` 
+        <div class="poster__bg"><img src="/public/Rectangle 229.png"" alt="">
+            <div class="player"><img src="/public/player.png" alt=""></div>
+        </div>
+        <div class="title__box">
+            <p>$35,00 <br>
+                ARTIST STARTER KIT</p>
+
+            <img width="40%" src="/public/stars (2).png" alt="">
+        </div>    
+    
+<div class="btn__cont">
+        <button class="basket__btn">В корзину </button>
+        </div>`
+        portfolioFlex__box.append(main__poster__box, create__box)
+    }
+
+    create__box.onclick = () => {
+        modal_create_prseta.style.display = "block"
+    }
+}
+portfolioPage()
+
+
+
+
+function demoPage(arr) {
+    let portfolioFlex__box = document.querySelector('.demo-flex__box')
+    let create__box = document.querySelector('.demo-create__box')
+
+    for (let item = 0; item < 3; item++) {
+        let main__poster__box = document.createElement('div')
+        main__poster__box.classList.add("main__poster__box")
+        main__poster__box.innerHTML = ` 
+        <div class="poster__bg"><img src="/public/Rectangle 28.png"" alt="">
+            <div class="player"><img src="/public/player.png" alt=""></div>
+        </div>
+        <div class="title__box">
+            <p>$35,00 <br>
+                ARTIST STARTER KIT</p>
+
+            <img width="40%" src="/public/stars (2).png" alt="">
+        </div>    
+    
+<div class="btn__cont">
+        <button class="basket__btn">В корзину </button>
+        </div>`
+        portfolioFlex__box.append(main__poster__box, create__box)
+    }
+    create__box.onclick = () => {
+        modal_create_prseta.style.display = "block"
+    }
+}
+demoPage()
